44e0408a091c60baafabdbe52ad27037a6eed4fe
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-12-02 19:54:54 +0100

    cr
    cr

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const uriDb = process.env.DB_URI;
+const PORT = process.env.PORT || 3000;
+
+const connection = mongoose.connect(uriDb, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+connection
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(
+				`Database connection successful. Use our API on port: ${PORT}`
+			);
+		});
+	})
+	.catch((err) => {
+		console.log(`Server not running. Error message: ${err.message}`);
+		process.exit(1);
+	});
