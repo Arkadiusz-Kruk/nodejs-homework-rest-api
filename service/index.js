2fa07785a2bfea0b85ba3756c8a2ddc4c52ba559
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-12-02 19:37:32 +0100

    c
    c

---
+++ service/index.js
@@ -0,0 +1,66 @@
+const {
+	Types: { ObjectId },
+} = require("mongoose");
+const Contact = require("./schemas/contact");
+
+const getAllContacts = async (id, page, limit, favorite) => {
+	if (favorite === undefined) {
+		return await Contact.find({ owner: id })
+			.limit(limit * 1)
+			.skip((page - 1) * limit);
+	}
+	return await Contact.find({ owner: id, favorite })
+		.limit(limit * 1)
+		.skip((page - 1) * limit);
+};
+
+const getOneContact = async (contactId) => {
+	let objectIdContactId;
+	try {
+		objectIdContactId = ObjectId(contactId);
+	} catch (error) {
+		return null;
+	}
+	return Contact.findOne({ _id: objectIdContactId }).lean();
+};
+
+const createContact = async (body, id) =>
+	Contact.create({ ...body, owner: id });
+
+const deleteContact = async (contactId) => {
+	let objectIdContactId;
+	try {
+		objectIdContactId = ObjectId(contactId);
+	} catch (error) {
+		return null;
+	}
+	return Contact.deleteOne({ _id: objectIdContactId });
+};
+
+const updateContact = async (contactId, body) => {
+	let objectIdContactId;
+	try {
+		objectIdContactId = ObjectId(contactId);
+	} catch (error) {
+		return null;
+	}
+	return Contact.findOneAndUpdate(
+		{
+			_id: objectIdContactId,
+		},
+		{ $set: body },
+		{
+			new: true,
+			runValidators: true,
+			strict: "throw",
+		}
+	);
+};
+
+module.exports = {
+	getAllContacts,
+	getOneContact,
+	createContact,
+	deleteContact,
+	updateContact,
+};
