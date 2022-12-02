9c0d2e94ce9b1be30263633ebf19d5caa71cd3bc
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-12-02 14:24:00 +0100

    a
    a

--- server.js
+++ server.js
@@ -1,5 +1,68 @@
-const app = require('./app')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const indexRouter = require("./routes/api/index");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+require("./config/passport");
+
+app.use("/api", indexRouter);
+
+app.use((req, res) => {
+	res.status(404).json({
+		status: "error",
+		code: 404,
+		message: "Use api on routes: /api/contacts",
+		message: `Use api on routes: 
+    /api/users/signup - registration user { email, password},
+    /api/users/login - login {email, password},
+	/api/users/logout - logout,
+	/api/users/current - current user,
+	/api/users/ - to change subscription,
+    /api/contacts - to get all users or post user,
+	/api/contacts/:contactId - to get, delete or update user by ID,
+	/api/contacts/:contactId/favorite - to add or remove user from favorites`,
+		data: "Not found",
+	});
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).json({
+		status: "fail",
+		code: 500,
+		message: err.message,
+		data: "Internal Server Error",
+	});
+});
+
+const uriDb = process.env.DB_URI;
+const PORT = process.env.PORT || 3000;
+
+const connection = mongoose.connect(uriDb, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+connection
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(
+				`Database connection successful. Use our API on port: ${PORT}`
+			);
+		});
+	})
+	.catch((err) => {
+		console.log(`Server not running. Error message: ${err.message}`);
+		process.exit(1);
+	});
